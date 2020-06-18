c257a3f69a829bbd521ab5afd441d9459b9f3216
Hayden Sartoris <hsartoris@gmail.com>
2020-06-18 09:15:47 -0400

    tangle dep

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [macroz/tangle "0.2.2"]]
+                 ;[metasoarous/oz "1.6.0-alpha6"]]
   :main ^:skip-aot cas-webflow-viz.core
+  :repl-options {:init-ns cas-webflow-viz.tangle}
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
